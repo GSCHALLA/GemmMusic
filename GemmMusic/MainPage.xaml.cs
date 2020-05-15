a610c11e18a7ede843d29ada962ad1086221acea
GSCHALLA <64715559+GSCHALLA@users.noreply.github.com>
2020-05-14 17:44:56 -0700

    Initial Commit

---
+++ GemmMusic/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using GemmMusic.Model;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Media.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace GemmMusic
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private ObservableCollection<Music> songs;
+        private List<MenuItem> MenuItems;
+        private Music currentSong;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            songs = new ObservableCollection<Music>();
+            MusicManager.GetAllSongs(songs);
+
+            MenuItems = new List<MenuItem>();
+            MenuItems.Add(new MenuItem { IconFile = "Assets/Icons/bruno.png", Category = MusicCategory.Brunos });
+            MenuItems.Add(new MenuItem { IconFile = "Assets/Icons/demi.png", Category = MusicCategory.Demis });
+            MenuItems.Add(new MenuItem { IconFile = "Assets/Icons/drake.png", Category = MusicCategory.Drakes });
+            MenuItems.Add(new MenuItem { IconFile = "Assets/Icons/selena.png", Category = MusicCategory.Selenas });
+
+            //BackButton.Visibility = Visibility.Collapsed;
+        }
+
+        private void MenuItemList_ItemClick(object sender, ItemClickEventArgs e)
+        {
+
+        }
+
+        private void SoundGridView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            var song = (Music)e.ClickedItem;
+            MyMediaElement.Source = new Uri(BaseUri, song.AudioFile);
+        }
+
+        private void HamBurgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+        }
+    }
+}
---
+++ GemmMusic/Model/MusicManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GemmMusic.Model
+{
+    public static class MusicManager
+    {
+        public static void GetAllSongs(ObservableCollection<Music> songs)
+        {
+            var allSongs = getSongs();
+            songs.Clear();
+
+            getSongs().ForEach(song => songs.Add(song));
+
+        }
+
+        private static List<Music> getSongs()
+        {
+            var songs = new List<Music>();
+            songs.Add(new Music("ShapeOfYou", MusicCategory.Brunos));
+
+            return songs;
+
+        }
+    }
+}
